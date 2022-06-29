c9a0d0f6991a28946d43c367cd82bd293d6856e3
Tobias Buschor <tobias.buschor@shwups.ch>
2022-06-29 11:38:57 +0200

    June 29, 2022 11:38 AM

--- PointerObserver.js
+++ PointerObserver.js
@@ -21,14 +21,14 @@ export class PointerObserver {
         this.move = this.move.bind(this);
         this.stop = this.stop.bind(this);
 
-        if (!touching) {
-			el.addEventListener('mousedown', this.start); // not before mousedown when touching, (because touch is triggering mousedown also?)
-		}
+		el.addEventListener('mousedown', this.start);  // why mousedown when only options.touch? To be able to change options after creating the observer?
         el.addEventListener('touchstart', this.start, {passive: this.options.passive});
     }
 	start(e) {
-		if (e.type === 'mousedown'  && !this.options.mouse) return; // why is the "start" listener added at all?
-		if (e.type === 'touchstart' && !this.options.touch) return; // Be be able to change options after creating the observer?
+		if (e.type === 'mousedown'  && !this.options.mouse) return;
+		if (e.type === 'touchstart' && !this.options.touch) return;
+		if (e.type === 'touchstart') this.touching = true;
+		if (this.touching && e.type === 'mousedown') return;
 
 		let pointer = e;
 		if (e.touches) {
@@ -37,18 +37,21 @@ export class PointerObserver {
 			this.identifier = pointer.identifier;
 		}
 
+		this.abortCtrl = new AbortController();
+		let signal = this.abortCtrl.signal;
+
 		this.posStart = this.pos = {
 			x:pointer.pageX,
 			y:pointer.pageY
 		};
 		if (this.options.mouse) {
-            d.addEventListener('mousemove', this.move);
-            d.addEventListener('mouseup'  , this.stop);
-			d.addEventListener('dragstart', this.stop);
+            d.addEventListener('mousemove', this.move, {signal});
+            d.addEventListener('mouseup'  , this.stop, {signal});
+			d.addEventListener('dragstart', this.stop, {signal});
 		}
 		if (this.options.touch) {
-			d.addEventListener('touchmove', this.move, {passive: this.options.passive});
-            d.addEventListener('touchend' , this.stop);
+			d.addEventListener('touchmove', this.move, {signal, passive: this.options.passive});
+            d.addEventListener('touchend' , this.stop, {signal});
             //d.addEventListener('touchstart', gstart);
 		}
 		this.onstart && this.onstart(e);
@@ -88,13 +91,9 @@ export class PointerObserver {
 	stop(e) {
 		if (e.changedTouches && e.changedTouches[0].identifier !== this.identifier) return;
 		this.onstop && this.onstop(e);
-		d.removeEventListener('mousemove', this.move);
-		d.removeEventListener('mouseup'  , this.stop);
-		d.removeEventListener('dragstart', this.stop);
-        d.removeEventListener('touchmove', this.move);
-		d.removeEventListener('touchend' , this.stop);
-        //d.removeEventListener('touchstart', this.gstart);
+		this.abortCtrl.abort();
 		touching = false;
+		this.touching = false;
 	}
 
     get diff() { // should we direct calculate this before onmove? i think .startDiff is almost ever used
